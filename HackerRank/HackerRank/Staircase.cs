46cd7cb651e1ba04944beee6f4cb5dbefe4e5bb1
Erhan <elverenerhan@gmail.com>
2017-02-06 17:18:40 +0300

    First Commit.

---
+++ HackerRank/HackerRank/Staircase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HackerRank
+{
+    public class Staircase
+    {
+        public static void StairCase(int n)
+        {
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine(new string('#', i).PadRight(n, ' '));
+        }
+    }
+}
