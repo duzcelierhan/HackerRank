51a69ec22d1866345503d11e7437e7be340d232e
Erhan <elverenerhan@gmail.com>
2017-02-09 11:55:19 +0300

    New chellange added

---
+++ HackerRank/CircularArrayRotation/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularArrayRotation
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            int q = Convert.ToInt32(tokens_n[2]);
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+            for (int i = 0; i < q; i++)
+            {
+                int m = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(a[(m - k + n + n) % n]);
+            }
+        }
+    }
+}
