0969d6596831de156121d7a8b50f6cc079fc58c3
Erhan ELVEREN <duzcelierhan@gmail.com>
2017-02-21 22:46:32 +0300

    Matrix Rotation is solved with an alternative

---
+++ HackerRank/MatrixLayerRotation/Alternative01.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace MatrixLayerRotation
+{
+    class Alternative01
+    {
+        enum Direction { None = 0, Down, Right, Up, Left }
+
+        private static int[,] matrix;
+        private static int[,] vertical;
+        private static int[,] horizontal;
+
+
+        static void Main(string[] args)
+        {
+            int[] parts = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int row = parts[0];
+            int col = parts[1];
+            int r = parts[2];
+
+            matrix = new int[row, col];
+
+            PrepareDirectionMatrix(row, col);
+
+            // Read and create the matrix
+            for (int i = 0; i < row; i++)
+            {
+                var line = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = line[j];
+                }
+            }
+
+            for (int i = 0; i < r; i++)
+            {
+                RotateMatrix();
+            }
+            PrintMatrix();
+        }
+
+        private static void PrepareDirectionMatrix(int row, int col)
+        {
+            vertical = new int[row, col];
+            horizontal = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (i >= j && (j < row - i - 1) && (j < col / 2))
+                        vertical[i, j] = 1;
+                    else if ((j >= row - i - 1) && (i >= row / 2) && (j < col - row + i))
+                        horizontal[i, j] = 1;
+                    else if ((j >= col - row + i) && (j >= col / 2) && (i >= col - j))
+                        vertical[i, j] = -1;
+                    else if ((j > i) && (i < row / 2) && (j < col - i))
+                        horizontal[i, j] = -1;
+                }
+            }
+        }
+
+        private static void RotateMatrix()
+        {
+            int row = matrix.GetLength(0);
+            int col = matrix.GetLength(1);
+            int[,] newMatrix = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    newMatrix[i + vertical[i, j], j + horizontal[i, j]] = matrix[i, j];
+                }
+            }
+            matrix = newMatrix;
+        }
+
+        private static void PrintMatrix()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- HackerRank/MatrixLayerRotation/Program.cs
+++ HackerRank/MatrixLayerRotation/Program.cs
@@ -1,37 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MatrixLayerRotation
 {
     class Program
     {
-        enum Direction{Down, Right, Up, Left}
+        enum Direction { None = 0, Down, Right, Up, Left }
 
         private static Direction[,] directionMatrix;
+        private static int[,] matrix;
 
-        static void Main(string[] args)
+        private static Dictionary<Direction, string> characters = new Dictionary<Direction, string>
         {
-            int[] parts = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-            int row = parts[0];
-            int col = parts[1];
-            int r = parts[2];
+            { Direction.Down, "↓" },
+            { Direction.Right, "→" },
+            { Direction.Up, "↑" },
+            { Direction.Left, "←" },
+            { Direction.None, "⦿" },
+        };
 
-            int[,] matrix = new int[row,col];
+        //static void Main(string[] args)
+        //{
+        //    int[] parts = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        //    int row = parts[0];
+        //    int col = parts[1];
+        //    int r = parts[2];
 
-            // Read and create the matrix
-            for (int i = 0; i < row; i++)
-            {
-                var line = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-                for (int j = 0; j < col; j++)
-                {
-                    matrix[i, j] = line[j];
-                }
-            }
+        //    matrix = new int[row, col];
 
-        }
+        //    PrepareDirectionMatrix(row, col);
+        //    //PrintDirectionMatrix();
+
+        //    // Read and create the matrix
+        //    for (int i = 0; i < row; i++)
+        //    {
+        //        var line = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        //        for (int j = 0; j < col; j++)
+        //        {
+        //            matrix[i, j] = line[j];
+        //        }
+        //    }
+
+        //    for (int i = 0; i < r; i++)
+        //    {
+        //        RotateMatrix();
+        //    }
+        //    PrintMatrix();
+        //}
 
         private static void PrepareDirectionMatrix(int row, int col)
         {
@@ -41,11 +57,63 @@ namespace MatrixLayerRotation
             {
                 for (int j = 0; j < col; j++)
                 {
-                    if(i >= j && i < row-j-1 && i < col-i-1)
-                        directionMatrix[i,j] = Direction.Down;
-                    else if()
+                    if (i >= j && (j < row - i - 1) && (j < col / 2))
+                        directionMatrix[i, j] = Direction.Down;
+                    else if ((j >= row - i - 1) && (i >= row / 2) && (j < col - row + i))
+                        directionMatrix[i, j] = Direction.Right;
+                    else if ((j >= col - row + i) && (j >= col / 2) && (i >= col - j))
+                        directionMatrix[i, j] = Direction.Up;
+                    else if ((j > i) && (i < row / 2) && (j < col - i))
+                        directionMatrix[i, j] = Direction.Left;
                 }
             }
+        }
+
+        private static void RotateMatrix()
+        {
+            int row = matrix.GetLength(0);
+            int col = matrix.GetLength(1);
+            int[,] newMatrix = new int[row, col];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    var dir = directionMatrix[i,j];
+                    newMatrix[i + (dir == Direction.Down ? 1 : (dir == Direction.Up) ? -1 : 0), j + (dir == Direction.Right ? 1 : dir == Direction.Left ? -1 : 0)] = matrix[i, j];
+                }
+            }
+            matrix = newMatrix;
+        }
+
+        private static void PrintMatrix()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        private static void PrintDirectionMatrix()
+        {
+            var encoding = Console.OutputEncoding;
+            Console.OutputEncoding = Encoding.Unicode;
+            for (int i = 0; i < directionMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < directionMatrix.GetLength(1); j++)
+                {
+                    Console.Write(characters[directionMatrix[i, j]]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.OutputEncoding = encoding;
         }
     }
 }
